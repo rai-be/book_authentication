55a150d254f1f8f7c3696229c3882b960fc4e743
raiani <raiani.guimaraes@zup.com.br>
2024-11-18 09:38:49 -0300

    creates authentication controller with login endpoint

---
+++ src/main/java/com/jtw/controller/ControllerAuth.java
@@ -0,0 +1,23 @@
+package com.jtw.controller;
+
+import com.jtw.controller.dto.DtoRequest;
+import com.jtw.controller.dto.DtoRespose;
+import com.jtw.service.ServiceAuth;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/auth")
+public class ControllerAuth {
+
+    private final ServiceAuth authService;
+
+    public ControllerAuth(ServiceAuth authService) {
+        this.authService = authService;
+    }
+    @PostMapping("/login")
+    @ResponseStatus(HttpStatus.OK)
+    public DtoRespose login(@RequestBody DtoRequest dtoRequest) {
+        return authService.authenticate(dtoRequest);
+    }
+}
