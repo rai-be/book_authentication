9459b0ad8df903a1fe4935a19969b8a31c96a921
raiani <raiani.guimaraes@zup.com.br>
2024-11-18 11:59:29 -0300

    feat: implements user details service for authentication

---
+++ src/main/java/com/jtw/service/CustomUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.jtw.service;
+
+import com.jtw.model.User;
+import com.jtw.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+        return new org.springframework.security.core.userdetails.User((String) user.getUsername(), (String) user.getPassword(), new ArrayList<>());
+    }
+}
